995263c5f3e40eb299e1fc18f3ab9c6737b4d5c5
oldp9 <tungldinfo@gmail.com>
2025-11-18 22:58:31 +0700

    a

--- bot.js
+++ bot.js
@@ -4,9 +4,11 @@ import convertBot from "./Bot/convert-Img/convertBot.js";
 import dotenv from 'dotenv';
 dotenv.config();
 
-const Main = () => {
+const Main = async () => {
     const TOKEN = process.env.BOT_TOKEN;
-    const bot = new TelegramBot(TOKEN, { polling: true });
+    const bot = new TelegramBot(TOKEN, { polling: {autoStart:false} });
+    await bot.getUpdates({offset: -1});
+    bot.startPolling();
     bot.setMyCommands([
         { command: "/system", description: "System" },
         { command: "/png", description: "Chuyển sang định dạng PNG" },
