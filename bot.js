994b230e86734d66a1c0c4b7c82425bded9870bf
oldp9 <tungldinfo@gmail.com>
2025-11-18 23:18:38 +0700

    q

--- bot.js
+++ bot.js
@@ -2,12 +2,13 @@ import TelegramBot from "node-telegram-bot-api";
 import RaspiBot from "./Bot/Raspi/raspiBot.js";
 import convertBot from "./Bot/convert-Img/convertBot.js";
 import dotenv from 'dotenv';
+import fetch from "node-fetch";
 dotenv.config();
 
 const Main = async () => {
     const TOKEN = process.env.BOT_TOKEN;
-    const bot = new TelegramBot(TOKEN, { polling: {autoStart:false} });
-    await bot.getUpdates({offset: -1});
+    const bot = new TelegramBot(TOKEN, { polling:false });
+    await fetch(`https://api.telegram.org/bot${TOKEN}/getUpdates?offset=-1`);
     bot.startPolling();
     bot.setMyCommands([
         { command: "/system", description: "System" },
